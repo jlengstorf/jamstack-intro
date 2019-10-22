0e90971bcecf8bc374324df9c13a607b4a8e11ef
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 16:20:07 -0500

    feat(todos): add support for deletion

--- sections/todos/src/components/todo.js
+++ sections/todos/src/components/todo.js
@@ -13,6 +13,10 @@ const Todo = ({ todo, reloadTodos }) => {
       .then(reloadTodos);
   };
 
+  const handleDelete = () => {
+    axios.post('/api/delete-todo', { id: todo._id }).then(reloadTodos);
+  };
+
   return (
     <>
       <label htmlFor={`todo-toggle-${todo._id}`} className={styles.label}>
@@ -29,6 +33,15 @@ const Todo = ({ todo, reloadTodos }) => {
       <p className={`${styles.text} ${todo.completed && styles.completed}`}>
         {todo.text}
       </p>
+
+      <label htmlFor={`todo-delete-${todo._id}`} className={styles.label}>
+        delete
+      </label>
+      <button onClick={handleDelete} className={styles.delete}>
+        <span role="img" aria-label="delete" title="delete this todo">
+          ❌
+        </span>
+      </button>
     </>
   );
 };
