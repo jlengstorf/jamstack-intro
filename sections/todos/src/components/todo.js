7234aea7ee5b4eeb47b799f39f3ccbf7a1fdcb2c
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 01:21:33 -0500

    feat(todos): add ability to toggle completed status

--- sections/todos/src/components/todo.js
+++ sections/todos/src/components/todo.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import styles from './todo.module.css';
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, reloadTodos }) => {
+  const toggleCompleted = () => {
+    axios
+      .post('/api/toggle-completed', {
+        id: todo._id,
+        text: todo.text,
+        completed: !todo.completed
+      })
+      .then(reloadTodos);
+  };
+
   return (
     <>
+      <label htmlFor={`todo=${todo._id}`} className={styles.label}>
+        Mark Complete
+      </label>
+      <input
+        id={`todo-${todo._id}`}
+        type="checkbox"
+        checked={todo.completed}
+        onChange={toggleCompleted}
+        className={styles.toggle}
+      />
+
       <p className={`${styles.text} ${todo.completed && styles.completed}`}>
         {todo.text}
       </p>
--- sections/todos/src/pages/index.js
+++ sections/todos/src/pages/index.js
@@ -41,7 +41,7 @@ export default () => {
         <ul className={styles.todos}>
           {todos.map(todo => (
             <li key={todo._id} className={styles.todo}>
-              <Todo todo={todo} />
+              <Todo todo={todo} reloadTodos={reloadTodos} />
             </li>
           ))}
         </ul>
