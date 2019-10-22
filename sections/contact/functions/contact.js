dd64026865a6efa1c5775a2db1a8de2d58ed5cca
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 12:26:04 -0500

    feat(contact): working contact form

--- sections/contact/functions/contact.js
+++ sections/contact/functions/contact.js
@@ -1,8 +1,25 @@
-exports.handler = (event, _context, callback) => {
-  console.log({ event });
+require('dotenv').config();
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ boop: true })
+exports.handler = (event, _context, callback) => {
+  const mailgun = require('mailgun-js');
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  });
+
+  const data = JSON.parse(event.body);
+
+  const email = {
+    from: 'Jason Lengstorf <jason@lengstorf.com>',
+    to: `${data.name} <${data.email}>`,
+    subject: data.subject,
+    text: data.body
+  };
+
+  mg.messages().send(email, (error, response) => {
+    callback(error, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    });
   });
 };
