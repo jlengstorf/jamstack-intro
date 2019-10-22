03a6fd67f0900f6edc8fa574a41b4b161c42e61c
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 13:43:21 -0500

    feat(dashboard): add route components

---
+++ sections/dashboard/src/components/route-login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RouteLogin = () => (
+  <>
+    <h1>Log In or Sign Up</h1>
+    <button>Log In</button>
+  </>
+);
+
+export default RouteLogin;
---
+++ sections/dashboard/src/components/route-secret.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RouteSecret = () => <h1>This is super secret stuff!</h1>;
+
+export default RouteSecret;
