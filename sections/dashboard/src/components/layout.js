f65928aee28d3e639aa74962c4870b5ecab13921
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 00:12:39 -0500

    feat(dashboard): add authentication

--- sections/dashboard/src/components/layout.js
+++ sections/dashboard/src/components/layout.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
+import 'react-netlify-identity-widget/styles.css';
 
 import './layout.css';
 
 const Layout = ({ children }) => (
-  <>
+  <IdentityContextProvider url="https://fem-jamstack-dashboard.netlify.com">
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
     <main>{children}</main>
-  </>
+  </IdentityContextProvider>
 );
 
 export default Layout;
--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Router } from '@reach/router';
 import { navigate } from 'gatsby';
+import IdentityModal from 'react-netlify-identity-widget';
 import Layout from '../components/layout';
 import Profile from '../components/profile';
+import PrivateRoute from '../components/private-route';
 import RouteLogin from '../components/route-login';
 import RouteBase from '../components/route-base';
 import RouteSecret from '../components/route-secret';
 
 const Dashboard = ({ location }) => {
+  const [isVisible, setVisibility] = useState(false);
+  const showModal = () => setVisibility(true);
+
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate('/dashboard/login', { replace: true });
@@ -16,12 +21,16 @@ const Dashboard = ({ location }) => {
 
   return (
     <Layout>
-      <Profile />
+      <Profile showModal={showModal} />
       <Router>
-        <RouteLogin path="/dashboard/login" />
-        <RouteSecret path="/dashboard/secret" />
-        <RouteBase path="/dashboard/base" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
+        <PrivateRoute path="/dashboard/secret" component={RouteSecret} />
+        <PrivateRoute path="/dashboard/base" component={RouteBase} />
       </Router>
+      <IdentityModal
+        showDialog={isVisible}
+        onCloseDialog={() => setVisibility(false)}
+      />
     </Layout>
   );
 };
