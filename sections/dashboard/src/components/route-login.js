c889bf5714ff3d8d01d479b20a9c39f16c8bc24a
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 14:16:34 -0500

    feat(dashboard): working protected routes

--- sections/dashboard/src/components/route-login.js
+++ sections/dashboard/src/components/route-login.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useIdentityContext } from 'react-netlify-identity';
+import { navigate } from 'gatsby';
 
-const RouteLogin = () => (
-  <>
-    <h1>Log In or Sign Up</h1>
-    <button>Log In</button>
-  </>
-);
+const RouteLogin = ({ showModal }) => {
+  const identity = useIdentityContext();
+
+  if (identity && identity.isLoggedIn) {
+    navigate('/dashboard/secret', { replace: true });
+  }
+
+  return (
+    <>
+      <h1>Log In or Sign Up</h1>
+      <button onClick={showModal}>Log In</button>
+    </>
+  );
+};
 
 export default RouteLogin;
