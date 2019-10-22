63b8f0425898f63993cedd93692f3a617e32f2f5
Jason Lengstorf <jason@lengstorf.com>
2019-10-22 00:58:08 -0500

    feat(todos): add UI for displaying todos

---
+++ sections/todos/src/components/todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './todo.module.css';
+
+const Todo = ({ todo }) => {
+  return (
+    <>
+      <p className={`${styles.text} ${todo.completed && styles.completed}`}>
+        {todo.text}
+      </p>
+    </>
+  );
+};
+
+export default Todo;
