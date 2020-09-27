8e7b2c9c95eea575d10b4e0c52b371c42767ce53
skgyan <skgyan@gmail.com>
2020-09-27 16:50:57 -0500

    list of repos

---
+++ sections/basic/main.js
@@ -0,0 +1,25 @@
+// To use async/await, we need to wrap everything in a function.
+const listRepos = async username => {
+    const repos = await fetch(
+      `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+    )
+      .then(res => res.json())
+      .catch(error => console.error(error));
+  
+    const markup = repos
+      .map(
+        repo => `
+          <li>
+            <a href="${repo.html_url}">${repo.name}</a>
+            (⭐️ ${repo.stargazers_count})
+          </li>
+        `
+      )
+      .join('');
+  
+    const content = document.getElementById('content');
+  
+    content.innerHTML = `<ul>${markup}</ul>`;
+  };
+  
+  listRepos('jlengstorf');
