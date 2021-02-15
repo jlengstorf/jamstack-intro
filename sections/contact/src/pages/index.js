6b47f8b05550f4642601accf1a0bd70537c4dd14
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-15 09:55:09 +0530

    feat(contact): create form component

---
+++ sections/contact/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../components/form";
+import "../global.css";
+
+const IndexPage = () => (
+  <>
+    <h1>Send a message</h1>
+    <Form />
+  </>
+);
+
+export default IndexPage;
