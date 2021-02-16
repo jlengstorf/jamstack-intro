bd4c8e56f97b6f1d7a8e3e526f4a6bd61fbb0707
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-16 10:50:59 +0530

    feat(dashboard): set up routing

---
+++ sections/dashboard/src/components/layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "gatsby";
+import "./layout.css";
+
+const Layout = ({ children }) => (
+  <>
+    <header>
+      <Link to="/">JAMstack App</Link>
+    </header>
+    <main>{children}</main>
+  </>
+);
+
+export default Layout;
---
+++ sections/dashboard/src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <p>TODO: create a dashboard</p>
+    </Layout>
+  );
+};
+
+export default Dashboard;
