66c162521fc7f316bdb994801051417b53ad74a1
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-19 22:43:28 +0530

    feat(todos): :sparkes: add support for toggling todos

--- sections/todos/src/components/todo.js
+++ sections/todos/src/components/todo.js
@@ -1,11 +1,34 @@
 import React from "react";
 import styles from "./todo.module.css";
+import axios from "axios";
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, reloadTodos }) => {
+  const toggleCompleted = () => {
+    axios
+      .post("/api/toggle-completed", {
+        id: todo._id,
+        text: todo.text,
+        completed: !todo.completed,
+      })
+      .then(reloadTodos);
+  };
+
   return (
-    <p className={`${styles.text} ${todo.completed && styles.completed}`}>
-      {todo.text}
-    </p>
+    <>
+      <label htmlFor={`todo-toggle-${todo._id}`} className={styles.label}>
+        Mark Completed
+      </label>
+      <input
+        id={`todo-toggle-${todo._id}`}
+        type="checkbox"
+        checked={todo.checked}
+        onChange={toggleCompleted}
+        className={styles.toggle}
+      />
+      <p className={`${styles.text} ${todo.completed && styles.completed}`}>
+        {todo.text}
+      </p>
+    </>
   );
 };
 
