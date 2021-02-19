53d91b5c2dae296b13ae808aee7a5af51e9f06d1
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-02-19 10:17:28 +0530

    feat(todos): :sparkles: add support for creating todos

--- sections/todos/src/pages/index.js
+++ sections/todos/src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./index.module.css";
 import axios from "axios";
 import Todo from "../components/todo";
+import Form from "../components/form";
 
 export default () => {
   const [status, setStatus] = useState("loading");
@@ -30,9 +31,12 @@ export default () => {
     };
   }, [status]);
 
+  const reloadTodos = () => setStatus("loading");
+
   return (
     <main>
       <h1 className={styles.heading}>JAMstack Todos</h1>
+      <Form reloadTodos={reloadTodos} />
       {todos ? (
         <ul className={styles.todos}>
           {todos.map((todo) => (
